7068aafbc38340d223fe402b20685ff6b9a54b59
iinaamasum <iinaamasum@gmail.com>
2022-11-21 21:36:46 +0600

    compare graph and titile fix

--- lab1/graph_plot.py
+++ lab1/graph_plot.py
@@ -8,11 +8,20 @@ input      b       i       s
 20000    27.002  14.393  25.538  
  """
 x_axis = [5000, 10000, 15000, 20000]
-y_axis = [1.556, 6.033, 13.955, 25.538]
+y_axis_b = [1.556, 6.033, 13.955, 25.538]
+y_axis_i = [0.857, 3.557, 7.978, 14.978]
+y_axis_s = [1.690, 6.654, 14.732, 27.002]
 
+y_variable = {"red": y_axis_b, "green": y_axis_i, "blue": y_axis_s}
+# colors = ["red", "blue", "green"]
 
-plt.plot(x_axis, y_axis, marker="o", color="green")
-plt.xlabel("Selection Sort")
+# for key, val in y_variable.items():
+#     # print(key)
+#     plt.plot(x_axis, val, marker="o", color=key)
+
+plt.plot(x_axis, y_axis_s, marker="o", color="blue")
+
+plt.xlabel("Input Size")
 plt.ylabel("Time (Sec)")
-plt.title("Input Size")
+plt.title("Selection Sort")
 plt.show()
