7aacd3e7a17f88a090ade7cdb5663864092c2479
iinaamasum <iinaamasum@gmail.com>
2022-12-03 01:33:48 +0600

    Dijkstra algorithm completed

--- lab6/Sortest_path/Dijkstra.py
+++ lab6/Sortest_path/Dijkstra.py
@@ -1,28 +1,5 @@
 import sys
-from queue import PriorityQueue
 from heapq import heappush, heappop
-
-
-class MinHeap:
-    def __init__(self) -> None:
-        self.heap = []
-
-    def insertHeap(self, val):
-        heappush(self.heap, val)
-
-    def extractMin(self):
-        return heappop(self.heap)
-
-    def parent(self, pos):
-        return pos // 2
-
-    def decreaseKey(self, i, new_val):
-        self.heap[i] = new_val
-        while i != 0 and self.heap[self.parent(i)] > self.heap[i]:
-            self.heap[i], self.heap[self.parent(i)] = (
-                self.heap[self.parent(i)],
-                self.heap[i],
-            )
 
 
 class Graph:
@@ -47,25 +24,18 @@ class Graph:
 
         # taking result set
         S = []
-        Q = MinHeap()
+        Q = []
 
-        for key, _ in self.graph.items():
-            Q.insertHeap(key)
+        for key in self.graph.keys():
+            heappush(Q, key)
 
-        while True:
-            try:
-                u = Q.extractMin()
-            except:
-                return
+        while Q:
+            u = heappop(Q)
             S.append(u)
 
             for key, val in self.graph.items():
                 for edge in val["adjacent"]:
-                    is_relax = self.relax(key, edge[0], edge[1])
-                    if is_relax:
-                        Q.decreaseKey(
-                            edge[0], self.graph[edge[0]]["property"]["distance"]
-                        )
+                    self.relax(key, edge[0], edge[1])
 
     def relax(self, u, v, w) -> None:
         if (
@@ -76,10 +46,6 @@ class Graph:
                 self.graph[u]["property"]["distance"] + w
             )
             self.graph[v]["property"]["parent"] = u
-            return True
-
-        else:
-            return False
 
 
 if __name__ == "__main__":
@@ -90,8 +56,10 @@ if __name__ == "__main__":
         g.addEdge(u, v, w)
 
     source = int(input("Enter Source: "))
-    print(g.graph)
     g.dijkstra(source)
+    print("\nPrinting distance and parent: ")
+    for key, val in g.graph.items():
+        print(key, ":", val)
 
 
 """ 
@@ -100,4 +68,13 @@ input
 1 2 10
 1 3 20
 2 3 5
+
+6
+1 2 10
+1 3 20
+2 3 40
+3 4 10
+5 4 5
+3 5 2
+1
 """
